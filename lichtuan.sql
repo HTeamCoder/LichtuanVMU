98f24c66c03e81b44186ae77c233209ce82c5e47
Phan Thanh <supersonic.kun@gmail.com>
2016-04-25 09:11:55 -0700

    csdl lịch tuần

---
+++ lichtuan.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 25, 2016 at 06:03 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `lichtuanvmu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lichtuan`
+--
+
+CREATE TABLE IF NOT EXISTS `lichtuan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tungay` date DEFAULT NULL,
+  `dengnay` date DEFAULT NULL,
+  `tuantheonam` varchar(155) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `tuannamtheonamhoc` varchar(155) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
